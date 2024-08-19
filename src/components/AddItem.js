974077d90ed5ede63a7d354d7170d9ee54947f69
Ellinor <ellinorekmark@hotmail.com>
2024-08-19 13:57:22 +0200

    bulk add items to list bonus: no more warning about missing keys in list

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,12 +1,19 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Box, Button, TextField} from "@mui/material";
+import React, { useEffect, useRef, useState } from 'react';
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Box,
+    Button,
+    TextField,
+    Typography
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
-
-const AddItem = ({uList, updateList}) => {
+const AddItem = ({ uList, updateList }) => {
     const [newItem, setItem] = useState('');
-    const textRef = useRef()
-
+    const [bulkItems, setBulkItems] = useState('');
+    const textRef = useRef();
 
     useEffect(() => {
         if (textRef.current) {
@@ -14,47 +21,61 @@ const AddItem = ({uList, updateList}) => {
         }
     }, []);
 
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        addToList()
-    }
+        addOne();
+    };
 
-
-
-    const addToList = () => {
-        if (newItem.trim() === '') return;
+    function addToList(i, updatedList) {
         const newItemObj = {
             listId: uList.listInfo.id,
-            item: newItem
+            item: i,
+            itemOrder: updatedList.items.length + 1 // Update itemOrder to reflect current length
         };
 
-        const updatedList = {
-            ...uList,
-            items: [...uList.items, newItemObj]
+        return {
+            ...updatedList,
+            items: [...updatedList.items, newItemObj]
         };
+    }
 
+    const addOne = () => {
+        if (newItem.trim() === '') return;
+
+        let updatedList = addToList(newItem, uList);
         updateList(updatedList);
 
         setItem('');
         textRef.current.focus();
     };
 
+    function bulkAdd() {
+        let updatedList = { ...uList };
+
+        const items = bulkItems.split(/\r?\n/);
+        items.forEach(i => {
+            if (i.trim() !== '') {
+                updatedList = addToList(i, updatedList); // Add each item to the updated list
+            }
+        });
+
+        updateList(updatedList); // Call updateList only once with all items added
+        setBulkItems(''); // Clear the bulk input field
+    }
+
     return (
         <>
             <form onSubmit={handleSubmit}>
-            <Box sx={{display: 'flex', alignItems: 'center', mt: 2}}>
-
+                <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
                     <TextField
-                        inputProps={{maxLength: 200}}
-
+                        inputProps={{ maxLength: 200 }}
                         id="new-item"
                         label="New Item"
                         variant="outlined"
                         value={newItem}
                         onChange={(e) => setItem(e.target.value)}
                         fullWidth
-                        sx={{height: '56px',}}
+                        sx={{ height: '56px', }}
                         inputRef={textRef}
                         InputProps={{
                             sx: {
@@ -65,7 +86,6 @@ const AddItem = ({uList, updateList}) => {
                                     borderBottomRightRadius: 0,
                                 },
                                 height: '56px',
-
                                 borderColor: 'primary.darker',
                             },
                         }}
@@ -80,19 +100,53 @@ const AddItem = ({uList, updateList}) => {
                             minWidth: '56px',
                         }}
                     >
-                        <AddIcon/>
+                        <AddIcon />
                     </Button>
-
-            </Box>
+                </Box>
             </form>
-
+            <br />
+            <Accordion sx={{
+                mx: 'auto',
+                mt: 2,
+                border: '1px solid',
+                borderColor: 'primary.darker',
+                borderRadius: 2,
+            }}>
+                <AccordionSummary>
+                    <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                        <Typography fontSize={"large"} color={'primary.main'}>Add Items in Bulk</Typography>
+                    </Box>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography fontSize={"small"} color={'primary.darker'} sx={{ paddingBottom: 1 }}>
+                        Separate items by new line.
+                    </Typography>
+                    <TextField
+                        inputProps={{ maxLength: 200 }}
+                        id="bulk-add"
+                        label="Bulk add"
+                        variant="outlined"
+                        value={bulkItems}
+                        onChange={(e) => setBulkItems(e.target.value)}
+                        fullWidth
+                        multiline
+                        minRows={3}
+                        InputProps={{
+                            sx: {
+                                height: '112px',
+                                borderColor: 'primary.darker',
+                            },
+                        }}
+                    />
+                    <Button
+                        variant={"outlined"}
+                        p={1}
+                        fullWidth
+                        onClick={bulkAdd}>Add items</Button>
+                </AccordionDetails>
+            </Accordion>
         </>
     );
 };
 
 export default AddItem;
-
-
-
-
-
