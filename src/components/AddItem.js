b9ba2457b6906e812e32751dd46553938193b6ea
Ellinor <ellinorekmark@hotmail.com>
2024-08-19 12:26:04 +0200

    press enter to add item, also textField remains focused when adding item either through enter or button press

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,10 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Box, Button, TextField} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
 
 const AddItem = ({uList, updateList}) => {
     const [newItem, setItem] = useState('');
+    const textRef = useRef()
+
+
+    useEffect(() => {
+        if (textRef.current) {
+            textRef.current.focus();
+        }
+    }, []);
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addToList()
+    }
+
+
 
     const addToList = () => {
         if (newItem.trim() === '') return;
@@ -21,48 +37,54 @@ const AddItem = ({uList, updateList}) => {
         updateList(updatedList);
 
         setItem('');
+        textRef.current.focus();
     };
 
     return (
         <>
-
+            <form onSubmit={handleSubmit}>
             <Box sx={{display: 'flex', alignItems: 'center', mt: 2}}>
-                <TextField
-                    inputProps={{maxLength: 200}}
-                    id="new-item"
-                    label="New Item"
-                    variant="outlined"
-                    value={newItem}
-                    onChange={(e) => setItem(e.target.value)}
-                    fullWidth
-                    sx={{height: '56px',}}
-                    InputProps={{
-                        sx: {
-                            borderTopRightRadius: 0,
-                            borderBottomRightRadius: 0,
-                            '& .MuiOutlinedInput-notchedOutline': {
+
+                    <TextField
+                        inputProps={{maxLength: 200}}
+
+                        id="new-item"
+                        label="New Item"
+                        variant="outlined"
+                        value={newItem}
+                        onChange={(e) => setItem(e.target.value)}
+                        fullWidth
+                        sx={{height: '56px',}}
+                        inputRef={textRef}
+                        InputProps={{
+                            sx: {
                                 borderTopRightRadius: 0,
                                 borderBottomRightRadius: 0,
-                            },
-                            height: '56px',
+                                '& .MuiOutlinedInput-notchedOutline': {
+                                    borderTopRightRadius: 0,
+                                    borderBottomRightRadius: 0,
+                                },
+                                height: '56px',
 
-                            borderColor: 'primary.darker',
-                        }
-                    }}
-                />
-                <Button
-                    onClick={addToList}
-                    variant="outlined"
-                    sx={{
-                        borderTopLeftRadius: 0,
-                        borderBottomLeftRadius: 0,
-                        height: '56px',
-                        minWidth: '56px',
-                    }}
-                >
-                    <AddIcon/>
-                </Button>
+                                borderColor: 'primary.darker',
+                            },
+                        }}
+                    />
+                    <Button
+                        type={"submit"}
+                        variant="outlined"
+                        sx={{
+                            borderTopLeftRadius: 0,
+                            borderBottomLeftRadius: 0,
+                            height: '56px',
+                            minWidth: '56px',
+                        }}
+                    >
+                        <AddIcon/>
+                    </Button>
+
             </Box>
+            </form>
 
         </>
     );
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "/api/";
-//export const BASE_URL = "http://localhost:8080/api/";
-export const BASE_URL_SOCKET = "/ws/";
-//export const BASE_URL_SOCKET = "http://localhost:8080/ws/";
+//export const BASE_URL = "/api/";
+export const BASE_URL = "http://localhost:8080/api/";
+//export const BASE_URL_SOCKET = "/ws/";
+export const BASE_URL_SOCKET = "http://localhost:8080/ws/";
